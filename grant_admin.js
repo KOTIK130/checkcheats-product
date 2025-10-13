b83701e7564df4cb92c77cb0ee05555482893da2
Сергей Сокольчук <prorok161009@gmail.com>
2025-10-13 17:09:55 +0300

    Update to BLUE theme, change price to , add cosmic logo

---
+++ grant_admin.js
@@ -0,0 +1,7 @@
+﻿const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./checkcheats.db");
+db.run("UPDATE users SET role = ? WHERE email = ?", ["admin", "prorok161009@gmail.com"], (err) => {
+    if (err) console.error(err);
+    else console.log("Admin role granted!");
+    db.close();
+});
